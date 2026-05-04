5d633b1f019829aa4f2008fa43e86a769b4038f9
benthecarman <benthecarman@live.com>
2026-05-04 12:02:59 -0500

    Pin libc to 0.2.183 on rustc < 1.65 in CI

--- fuzz/ci-fuzz.sh
+++ fuzz/ci-fuzz.sh
@@ -2,13 +2,22 @@
 set -e
 set -x
 
+RUSTC_MINOR_VERSION=$(rustc --version | awk '{ split($2,a,"."); print a[2] }')
+
+# Starting with version 0.5.60, the `honggfuzz` crate has an MSRV of rustc 1.70.
+# 0.5.59 nominally has an MSRV of 1.63, but its bundled Cargo.lock pulls in
+# transitive deps (`unicode-ident` 1.0.24) that require rustc 1.71. The 0.5.58
+# bundled lockfile resolves cleanly on 1.63, so use that with `--locked`.
+HONGGFUZZ_INSTALL_ARGS=""
+[ "$RUSTC_MINOR_VERSION" -lt 70 ] && HONGGFUZZ_INSTALL_ARGS='--locked --version=0.5.58'
+
 pushd src/bin
 rm *_target.rs
 ./gen_target.sh
 [ "$(git diff)" != "" ] && exit 1
 popd
 
-cargo install --color always --force honggfuzz --no-default-features
+cargo install --color always --force honggfuzz --no-default-features $HONGGFUZZ_INSTALL_ARGS
 sed -i 's/lto = true//' Cargo.toml
 
 export RUSTFLAGS="--cfg=secp256k1_fuzz --cfg=hashes_fuzz"
